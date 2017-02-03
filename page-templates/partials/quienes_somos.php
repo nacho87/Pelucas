40531667d8d2b78330ad91de303b149c172987f7
Nacho Decima <jidecima87@gmail.com>
2017-02-03 19:35:53 -0300

    Fix style for header and grid of products.

--- page-templates/partials/quienes_somos.php
+++ page-templates/partials/quienes_somos.php
@@ -6,12 +6,10 @@
 			<div class="description">
 				<?php echo get_field('description_about') ?>
 			</div>
-			<div class="map-section" data-behavior="map">
+			<div class="map-section" >
 
 				<?php if( !empty($location) ): ?>
-					<div class="acf-map">
-						<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
-					</div>
+
 				<?php endif; ?>
 			</div>
 		</div>
