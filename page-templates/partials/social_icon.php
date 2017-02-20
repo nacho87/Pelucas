75ceca694fbff0c2c09b0a3a68f658979ef49a97
Nacho Decima <jidecima87@gmail.com>
2017-02-20 11:54:27 -0300

    Fix links and icon for social links.

--- page-templates/partials/social_icon.php
+++ page-templates/partials/social_icon.php
@@ -1,7 +1,19 @@
 <div class="social">
 	<ul>
-		<li><a class="item-social" href=""><span class="icon-facebook"></span></a></li>
-		<li><a class="item-social" href=""><span class="icon-googleplus"></span></a></li>
-		<li><a class="item-social" href=""><span class="icon-twitter"></span></a></li>
+		<li>
+			<a class="item-social" href="<?php echo get_field( 'facebook_url' ,'option') ?>">
+				<span class="icon-<?php echo get_field( 'facebook_icon' ,'option') ?>"></span>
+			</a>
+		</li>
+		<li>
+			<a class="item-social" href="<?php echo get_field( 'google_url' ,'option') ?>">
+				<span class="icon-<?php echo get_field( 'google_icon' ,'option') ?>"></span>
+			</a>
+		</li>
+		<li>
+			<a class="item-social" href="<?php echo get_field( 'twitter_url' ,'option') ?>">
+				<span class="icon-<?php echo get_field( 'twitter_icon' ,'option') ?>"></span>
+			</a>
+		</li>
 	</ul>
 </div>
